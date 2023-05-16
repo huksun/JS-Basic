01faac551367b1b40f1fe0b7ec03bc281d74dcf9
HeroManPing <90021167+HeroManPing@users.noreply.github.com>
2023-05-17 00:03:01 +0700

    chapter 2 video 4

---
+++ Video3/video3.js
@@ -0,0 +1,25 @@
+// Number:
+let length = 16;    
+let weight = 7.5;
+
+// Strings:
+let color = '#000000';
+let lastName = "James";
+
+// Booleans:
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName: "Ngô", lastName: "Hùng"};
+
+//Arrays object:
+const cars = ["BMW", "Honda", "Maybach"]
+
+//Data objects:
+const date = new Date().getFullYear();
+
+let car = undefined;    //Value is undefined, type is undefined 
+let newcar;
+
+console.log(color, lastName);
